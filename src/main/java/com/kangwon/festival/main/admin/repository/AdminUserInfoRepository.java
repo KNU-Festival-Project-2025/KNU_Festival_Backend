d06e9e3aad6ee33f7dab1833f7064246d13c0d67
ryudain <75834815+ryudain05@users.noreply.github.com>
2025-09-01 17:27:43 +0900

    Revert "Develop"

--- src/main/java/com/kangwon/festival/main/admin/repository/AdminUserInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kangwon.festival.main.admin.repository;
-
-import com.kangwon.festival.global.entity.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminUserInfoRepository extends JpaRepository<UserInfo, Integer> {
-
-}

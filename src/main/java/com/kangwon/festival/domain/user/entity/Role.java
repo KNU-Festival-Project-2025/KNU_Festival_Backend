3a166e5604c21ec6f957b67cde169b5302d6dc47
ssoyeonni <165532198+ssoyeonni@users.noreply.github.com>
2025-09-11 14:17:22 +0900

    Revert "feat: 카카오 로그인 구현 및 JWT 인증인가 처리"

--- src/main/java/com/kangwon/festival/domain/user/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.kangwon.festival.domain.user.entity;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- src/main/java/com/kangwon/festival/global/exception/InternalServerException.java
+++
@@ -1,9 +0,0 @@
-package com.kangwon.festival.global.exception;
-
-import static com.kangwon.festival.global.exception.Code.INTERNAL_SERVER_ERROR;
-
-public class InternalServerException extends BaseException{
-    public InternalServerException() {
-        super(INTERNAL_SERVER_ERROR);
-    }
-}
